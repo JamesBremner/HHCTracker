080ca498fd8b4325b46845eaf4b0fa12b7572bb0
JamesBremner <james@ravenspoint.com>
2022-09-13 10:49:56 -0400

    keep track of PIDs displayed

--- src/main.cpp
+++ src/main.cpp
@@ -25,19 +25,34 @@ class cDB
         std::string value;
     };
 
+    /// @brief A pair describing a person
+    /// first is the person's id
+    /// second is a vector of strings containing the person's attributes
+    typedef std::pair<
+        int, std::vector<std::string>>
+        person_t;
+
+    /// @brief A vector describing a subset of persons
+    typedef std::vector<person_t> vperson_t;
+
 public:
     void addNurse(
         const std::string &name,
         const std::string &licence,
         const std::string &service);
+    void addPatient(
+        const std::string &name);
 
-    std::vector<std::string> nurse();
-
-    
+    vperson_t nurse();
+    person_t nurse(int pid);
+    vperson_t patient();
+    person_t patient(int pid);
 
 private:
     static int lastPID;
-    std::vector<sValue> myValue;
+    std::vector<sValue> myValue; /// the database
+    vperson_t myNurseList;       /// the displayed nurses
+    vperson_t myPatientList;     /// the displayed patients
 };
 
 int cDB::lastPID = -1;
@@ -56,46 +71,18 @@ public:
           lbNurseOrder(wex::maker::make<wex::label>(plNurse)),
           chNurseOrder(wex::maker::make<wex::choice>(plNurse)),
           lsNurse(wex::maker::make<wex::list>(plNurse)),
-          bnNurseAdd(wex::maker::make<wex::button>(plNurse))
+          bnNurseAdd(wex::maker::make<wex::button>(plNurse)),
+
+          bnPatientAdd(wex::maker::make<wex::button>(plPatient)),
+          lsPatient(wex::maker::make<wex::list>(plPatient))
     {
         tabs.move(0, 0, 1000, 500);
         tabs.tabWidth(200);
         tabs.add("Nurses", plNurse);
         tabs.add("Patients", plPatient);
 
-        plNurse.move(10, 10, 900, 400);
-
-        lbNursePanel.move(20, 40, 100, 30);
-        lbNursePanel.text("Nurses");
-
-        bnNurseAdd.move(200, 40, 100, 30);
-        bnNurseAdd.text("ADD");
-        bnNurseAdd.events().click(
-            [&]
-            {
-                addNurse();
-                listNurse();
-            });
-
-        lbNurseOrder.move(20, 70, 40, 30);
-        lbNurseOrder.text("Order");
-        chNurseOrder.move(80, 70, 100, 100);
-        chNurseOrder.add("Licence");
-        chNurseOrder.add("InService");
-        chNurseOrder.events().select(
-            chNurseOrder.id(),
-            [&]
-            {
-                listNurse();
-            });
-
-        lsNurse.move(10, 120, 900, 400);
-        lsNurse.events().select(
-            lsNurse.id(),
-            [&]
-            {
-                editNurse();
-            });
+        ConstructNursesPanel();
+        ConstructPatientsPanel();
 
         show();
         tabs.select(0);
@@ -110,13 +97,22 @@ private:
     wex::label &lbNurseOrder;
     wex::choice &chNurseOrder;
     wex::list &lsNurse;
+    wex::list &lsPatient;
     wex::button &bnNurseAdd;
 
+    wex::button &bnPatientAdd;
+
     cDB theDB;
+
+    void ConstructNursesPanel();
+    void ConstructPatientsPanel();
 
     void addNurse();
     void listNurse();
     void editNurse();
+
+    void addPatient();
+    void listPatient();
 };
 
 void cDB::addNurse(
@@ -139,29 +135,122 @@ void cDB::addNurse(
     v.value = service;
     myValue.push_back(v);
 }
-
-std::vector<std::string> cDB::nurse()
+void cDB::addPatient(
+    const std::string &name)
 {
-    std::vector<std::string> ret;
+    sValue v;
+    v.aid = eAttribute::name;
+    v.pid = ++lastPID;
+    v.value = name;
+    myValue.push_back(v);
+    v.aid = eAttribute::role;
+    v.value = "patient";
+}
+
+cDB::vperson_t cDB::nurse()
+{
+    myNurseList.clear();
     for (auto &v : myValue)
     {
         if (v.aid == eAttribute::role && v.value == "nurse")
         {
-            int pid = v.pid;
-            std::string desc;
-            for (auto &v2 : myValue)
-            {
-                if (v2.aid == eAttribute::name && v2.pid == pid)
-                    desc += v2.value + "  ";
-                if (v2.aid == eAttribute::nurselicence && v2.pid == pid)
-                    desc += v2.value + "  ";
-                if (v2.aid == eAttribute::inService && v2.pid == pid)
-                    desc += v2.value + "  ";
-            }
-            ret.push_back(desc);
+            myNurseList.push_back(
+                nurse(v.pid));
         }
     }
+    return myNurseList;
+}
+cDB::vperson_t cDB::patient()
+{
+    myPatientList.clear();
+    for (auto &v : myValue)
+    {
+        if (v.aid == eAttribute::role && v.value == "patient")
+        {
+            myPatientList.push_back(
+                patient(v.pid));
+        }
+    }
+    return myPatientList;
+}
+cDB::person_t cDB::nurse(int pid)
+{
+    person_t ret;
+    ret.first = pid;
+    ret.second.resize(3);
+    for (auto &v : myValue)
+    {
+        if (v.aid == eAttribute::name && v.pid == pid)
+            ret.second[0] = v.value;
+        if (v.aid == eAttribute::nurselicence && v.pid == pid)
+            ret.second[1] = v.value;
+        if (v.aid == eAttribute::inService && v.pid == pid)
+            ret.second[2] = v.value;
+    }
     return ret;
+}
+cDB::person_t cDB::patient(int pid)
+{
+    person_t ret;
+    ret.first = pid;
+    ret.second.resize(1);
+    for (auto &v : myValue)
+    {
+        if (v.aid == eAttribute::name && v.pid == pid)
+            ret.second[0] = v.value;  
+    }
+    return ret;  
+}
+
+void cGUI::ConstructNursesPanel()
+{
+    plNurse.move(10, 10, 900, 400);
+
+    lbNursePanel.move(20, 40, 100, 30);
+    lbNursePanel.text("Nurses");
+
+    bnNurseAdd.move(200, 40, 100, 30);
+    bnNurseAdd.text("ADD");
+    bnNurseAdd.events().click(
+        [&]
+        {
+            addNurse();
+            listNurse();
+        });
+
+    lbNurseOrder.move(20, 70, 40, 30);
+    lbNurseOrder.text("Order");
+    chNurseOrder.move(80, 70, 100, 100);
+    chNurseOrder.add("Licence");
+    chNurseOrder.add("InService");
+    chNurseOrder.events().select(
+        chNurseOrder.id(),
+        [&]
+        {
+            listNurse();
+        });
+
+    lsNurse.move(10, 120, 900, 400);
+    lsNurse.events().select(
+        lsNurse.id(),
+        [&]
+        {
+            editNurse();
+        });
+}
+
+void cGUI::ConstructPatientsPanel()
+{
+    plPatient.move(10, 10, 900, 400);
+
+    bnPatientAdd.move(200, 40, 100, 30);
+    bnPatientAdd.text("ADD");
+    bnPatientAdd.events().click(
+        [&]
+        {
+            addPatient();
+            listPatient();
+        });
 }
 
 void cGUI::addNurse()
@@ -179,19 +268,47 @@ void cGUI::addNurse()
         ib.value("Licence"),
         ib.value("Service"));
 }
+void cGUI::addPatient()
+{
+    wex::inputbox ib;
+    ib.text("Patient");
+    ib.add("Name", "");
 
+    ib.showModal();
+
+    theDB.addPatient(
+        ib.value("Name"));
+}
 void cGUI::listNurse()
 {
     lsNurse.clear();
     for (auto &n : theDB.nurse())
     {
-        lsNurse.add(n);
+        std::stringstream ss;
+        for (auto &s : n.second)
+        {
+            ss << std::setw(12) << s;
+        }
+        lsNurse.add(ss.str());
+    }
+}
+void cGUI::listPatient()
+{
+    lsPatient.clear();
+    for (auto &n : theDB.patient())
+    {
+        std::stringstream ss;
+        for (auto &s : n.second)
+        {
+            ss << std::setw(12) << s;
+        }
+        lsPatient.add(ss.str());
     }
 }
 
 void cGUI::editNurse()
 {
-    lsNurse.selectedIndex();
+    theDB.nurse(lsNurse.selectedIndex());
 }
 
 main()
